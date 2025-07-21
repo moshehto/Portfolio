5bd0473726dbd84d65080ac3c6917946da96ea81
mshehto <mshehto@uwo.ca>
2025-07-22 00:16:00 +0300

    Fixing chat.js file by reworking coors part2

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.options('*', cors()); // <-- allow CORS preflight for all routes
 app.use(express.json());
 app.use('/api/chat', chatRoute);
 
